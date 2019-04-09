d768245c6f6efa810920ba1afd96df60a6e43b24
Evgeny Kireev <>
2019-04-09 20:51:14 +0300

    Empty project

---
+++ Mazathoth/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Mazathoth
+//
+//  Created by Evgeny Kireev on 09/04/2019.
+//  Copyright © 2019 plasmon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Mazathoth/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Mazathoth
+//
+//  Created by Evgeny Kireev on 09/04/2019.
+//  Copyright © 2019 plasmon. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
