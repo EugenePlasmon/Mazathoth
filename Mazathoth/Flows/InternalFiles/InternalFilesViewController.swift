e808d04b611393f3a228f9c4815c6a386298c1c5
Nadezhda <zhuravleva.naden@gmail.com>
2019-04-19 21:49:30 +0300

    Deletе file, Add/Delete folder

--- Mazathoth/Flows/InternalFiles/InternalFilesViewController.swift
+++ Mazathoth/Flows/InternalFiles/InternalFilesViewController.swift
@@ -31,6 +31,7 @@ final class InternalFilesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.addSubviews()
+        self.addNavigationItem()
         self.loadDataFromDocumentDirectory()
     }
     
@@ -48,6 +49,45 @@ final class InternalFilesViewController: UIViewController {
         self.internalFilesView.frame = view.bounds
         self.internalFilesView.tableView.delegate = self
         self.internalFilesView.tableView.dataSource = self
+    }
+    
+    private func addNavigationItem() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.createFolder))
+    }
+}
+
+// MARK: - Alert
+
+extension InternalFilesViewController: UITextFieldDelegate {
+    
+    @objc private func createFolder() {
+        let alertController = UIAlertController(title: "Новая папка", message: "Присвойте название этой папке", preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Название"
+        }
+        let alertCancelAction = UIAlertAction(title: "Отмена", style: .cancel)
+        let alertSaveAction = UIAlertAction(title: "Сохранить", style: .default) { [weak alertController] _ in
+            guard let alertController = alertController, let textField = alertController.textFields?.first, let nameFolder = textField.text else { return }
+            self.addFolderToDocumentsFolder(whithName: nameFolder)
+            self.loadDataFromDocumentDirectory()
+            self.internalFilesView.tableView.reloadData()
+            }
+        alertController.addAction(alertCancelAction)
+        alertController.addAction(alertSaveAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    private func addFolderToDocumentsFolder(whithName name: String) {
+        let directories = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        guard let directory = directories.first else {
+            return
+        }
+        let absolutePath = (directory as NSString).appendingPathComponent(name)
+        do {
+            try FileManager.default.createDirectory(atPath: absolutePath, withIntermediateDirectories: false, attributes: nil)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
 }
 
@@ -70,6 +110,10 @@ extension InternalFilesViewController: UITableViewDelegate {
 
 extension InternalFilesViewController: UITableViewDataSource {
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return max(self.internalFiles.count, 1)
     }
@@ -86,5 +130,24 @@ extension InternalFilesViewController: UITableViewDataSource {
         }
         cell.internalFileLabel?.text = self.internalFiles[indexPath.row].name
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            guard !self.internalFiles.isEmpty else { return }
+            self.removeInternalFile(atPath: self.internalFiles[indexPath.row].absolutePath)
+            self.internalFiles.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    private func removeInternalFile(atPath absolutePath: String) {
+        guard FileManager.default.fileExists(atPath: absolutePath) else { return }
+        do {
+            try FileManager.default.removeItem(atPath: absolutePath)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            return
+        }
     }
 }
